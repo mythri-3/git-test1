b696bf558babcc56801ecbef60d03b04f5d42c34
mythri-3 <mgudiban@uncc.edu>
2024-01-22 10:43:52 -0500

    added files to dev

---
+++ uncc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC-Mythri Gudibanda</title>
+</head>
+<body>
+    <h1>Execute files on demand</h1>
+    <?php echo 9 * 13 ?>
+</body>
+</html>
